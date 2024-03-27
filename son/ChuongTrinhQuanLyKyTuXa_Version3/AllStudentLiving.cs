c0d778f3d6be055a4a72834bfa5977100aff1032
thanhbinhtest <hoangbinh15021998@gmail.com>
2024-03-27 13:50:47 +0700

    son

---
+++ son/ChuongTrinhQuanLyKyTuXa_Version3/AllStudentLiving.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ChuongTrinhQuanLyKyTuXa_Version3
+{
+    public partial class AllStudentLiving : Form
+    {
+        Dashboard dashboardForm = new Dashboard();
+        function fn = new function();
+        String query;
+        public AllStudentLiving()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            fn.close();
+        }
+
+        private void AllStudentLiving_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(450, 131);
+            query = "SELECT * FROM newStudent WHERE living = 'Yes'";
+            DataSet ds = fn.getData(query);
+            guna2DataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            fn.back(this, dashboardForm);
+        }
+    }
+}
